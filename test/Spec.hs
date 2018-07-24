b54f662e04b844eed2845ec7f16b3134b3680a3a
Dung Truong <revskill@Dungs-iMac.local>
2018-07-24 14:03:40 +0700

    Fix

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,13 +8,13 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = with (return (app "hi")) $ do
+spec = with (return (app "hi")) $
   describe "GET /ep1" $ do
-    it "responds with 200" $ do
-      get "/ep1" `shouldRespondWith` 200
-    it "responds with 1797" $ do
-      get "/ep1" `shouldRespondWith` "1797"
-    it "responds with 200" $ do
-      get "/ep2" `shouldRespondWith` 200
-    it "responds with hi" $ do
-      get "/ep2" `shouldRespondWith` "\"hi\""
+  it "responds with 200" $
+    get "/ep1" `shouldRespondWith` 200
+  it "responds with 1797" $
+    get "/ep1" `shouldRespondWith` "1797"
+  it "responds with 200" $
+    get "/ep2" `shouldRespondWith` 200
+  it "responds with hi" $
+    get "/ep2" `shouldRespondWith` "\"hi\""
